1e6fa86adf94a4acde4020aa865f70ed6684641f
TonRichards <nopparat4@gmail.com>
2025-05-03 11:07:06 +0000

    Remove project management service from sidebar menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -30,7 +30,6 @@
 
     <nav class="flex-1 p-4 space-y-2 text-sm">
       <SidebarItem :icon="ViewDashboardOutlineIcon" label="Dashboard" to="/dashboard" />
-      <SidebarItem :icon="FolderOutlineIcon" label="Project Management" to="/projects" />
       <SidebarItem :icon="ReceiptOutlineIcon" label="Orders" to="/orders" />
       <SidebarItem :icon="TruckOutlineIcon" label="Shipping" to="/shippings" />
 
@@ -56,7 +55,6 @@ import SidebarItem from './SidebarItem.vue'
 import SidebarCollapseItem from './SidebarCollapseItem.vue'
 
 import ViewDashboardOutlineIcon from 'vue-material-design-icons/ViewDashboardOutline.vue'
-import FolderOutlineIcon from 'vue-material-design-icons/FolderOutline.vue'
 import ReceiptOutlineIcon from 'vue-material-design-icons/ReceiptOutline.vue'
 import TruckOutlineIcon from 'vue-material-design-icons/TruckOutline.vue'
 import CogOutlineIcon from 'vue-material-design-icons/CogOutline.vue'
